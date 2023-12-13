ff5bdc1833d65f226238e8198ed9fd5bc8a098d3
Alexey <ale.vel.boom@gmail.com>
2023-12-13 18:53:30 +0300

    Делает все декоративные элементы

--- source/js/script.js
+++ source/js/script.js
@@ -1,22 +1,26 @@
-let navMain = document.querySelector('.page-navigation__list');
-let navToggle = document.querySelector('.page-navigation__toggle');
+let navMain = document.querySelector('.page-navigation');
+let navToggle = document.querySelector('.page-header__toggle-navigation');
+
+navToggle.classList.remove('page-header__toggle-navigation--nojs');
+
+navMain.classList.remove('page-navigation--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navToggle.classList.contains('page-navigation__toggle--close')) {
-    navToggle.classList.remove('page-navigation__toggle--close');
-    navToggle.classList.add('page-navigation__toggle--open');
+  if (navToggle.classList.contains('page-header__toggle-navigation--close')) {
+    navToggle.classList.remove('page-header__toggle-navigation--close');
+    navToggle.classList.add('page-header__toggle-navigation--open');
   } else {
-    navToggle.classList.add('page-navigation__toggle--close');
-    navToggle.classList.remove('page-navigation__toggle--open');
+    navToggle.classList.add('page-header__toggle-navigation--close');
+    navToggle.classList.remove('page-header__toggle-navigation--open');
   }
 });
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('page-navigation__list--close')) {
-    navMain.classList.remove('page-navigation__list--close');
-    navMain.classList.add('page-navigation__list--open');
+  if (navMain.classList.contains('page-navigation--close')) {
+    navMain.classList.remove('page-navigation--close');
+    navMain.classList.add('page-navigation--open');
   } else {
-    navMain.classList.add('page-navigation__list--close');
-    navMain.classList.remove('page-navigation__list--open');
+    navMain.classList.add('page-navigation--close');
+    navMain.classList.remove('page-navigation--open');
   }
 });
